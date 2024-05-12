5687421fb09e94fd31307ce05ae5cda186aabdb8
Meliketoaste <emilnord77@gmail.com>
2024-05-12 12:19:33 +0200

    fixed error i hope

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -1,10 +1,10 @@
 return { -- Collection of various small independent plugins/modules
-    {
-        "nvim-lualine/lualine.nvim",
-        -- You can optionally lazy-load heirline on UIEnter
-        event = "UIEnter",
-        config = function()
-            require "lua.plugins.extern.minim"
-        end,
-    },
+  {
+    'nvim-lualine/lualine.nvim',
+    -- You can optionally lazy-load heirline on UIEnter
+    event = 'UIEnter',
+    config = function()
+      require 'lua.plugins.minim'
+    end,
+  },
 }
