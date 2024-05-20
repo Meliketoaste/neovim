bae4068a7b29d0e47f7038046c4a38cdf33dc84e
Meliketoaste <emilnord77@gmail.com>
2024-05-20 18:53:29 +0200

    update

--- lua/plugins/bufferline.lua
+++ lua/plugins/bufferline.lua
@@ -1,17 +1,17 @@
 return {
   'akinsho/bufferline.nvim',
-  version = 'v3.*',
+  version = '*',
   dependencies = 'nvim-tree/nvim-web-devicons',
-  config = {
-    options = {
-      offsets = {
-        {
-          filetype = 'neo-tree',
-          text = 'NeoTree',
-          text_align = 'left',
-          separator = true,
-        },
-      },
-    },
-  },
+  --config = {
+  --  options = {
+  --    offsets = {
+  --      {
+  --        filetype = 'neo-tree',
+  --        text = 'NeoTree',
+  --        text_align = 'left',
+  --        separator = true,
+  --      },
+  --    },
+  --  },
+  --},
 }
